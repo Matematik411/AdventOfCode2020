99063bf6c7870fa9d4673394a921721446a97891
Matematik411 <nz3928@student.uni-lj.si>
2020-12-01 17:58:05 +0100

    day1

---
+++ day01.py
@@ -0,0 +1,43 @@
+list = [int(input()) for _ in range(200)]
+list.sort()
+
+stop = False
+
+i = 0
+j = 199
+while i < 199:
+    if stop:
+        break
+    while j-i>0:
+        if list[i] + list[j] == 2020:
+            print(list[i]*list[j])
+            stop = True
+            break
+        j -=1
+    i +=1
+    j = 199
+
+
+i = 0
+j = 0
+k = 199
+
+stop = False
+
+while i < 198:
+    if stop:
+        break
+    while j < 199:
+        if stop:
+            break
+        while k > j:
+            if list[i] + list[j] + list[k] == 2020:
+                print(list[i]*list[j]*list[k])
+                stop = True
+                break
+            k -=1
+        j +=1
+        k = 199
+    i +=1
+    j = i
+    k = 199
